740e4c84a94664027427491e5c95b2c931170db9
teacher <teacher@teacherdeMac.local>
2016-10-11 18:45:53 +0800

    Initial Commit

---
+++ ManGongXiHuo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ManGongXiHuo
+//
+//  Created by teacher on 16-10-11.
+//  Copyright (c) 2016年 teacher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ManGongXiHuo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ManGongXiHuo
+//
+//  Created by teacher on 16-10-11.
+//  Copyright (c) 2016年 teacher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
